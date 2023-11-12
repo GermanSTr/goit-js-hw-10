618d54e6b2b250707509f1b9e4ea7d1561c4db81
GermanSTr <gershev1994@gmail.com>
2023-11-13 00:30:30 +0200

    task-01-04

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,24 +1,19 @@
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_9p5DxSMgGFBSNpUnADSSOGhBmedziXgZyQg8b6E12ufPXByiIM3ZFe61d4GpuLEm';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
-const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT = '/breeds';
-const END_POINT_ID = 'images/search';
+const END_POINT_ID = '/images/search?';
 
 export function fetchBreeds() {
-  const url = BASE_URL + END_POINT;
-
-  return axios.get(url).then(res => res.data);
+  return axios.get(END_POINT).then(res => res.data);
 }
 
 export function fetchCatByBreed(breedId) {
-  const BASE_URL = 'https://api.thecatapi.com/v1';
-  const END_POINT_ID = '/images/search?';
   const PARAMS = new URLSearchParams({
     breed_ids: breedId,
   });
-
-  const url = BASE_URL + END_POINT_ID + PARAMS;
+  const url = END_POINT_ID + PARAMS;
   return axios.get(url).then(response => response.data);
 }
---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
+import { breedMarkup } from './markup';
+
+const selectBreedEl = document.querySelector('.js-breed-select');
+const catInfo = document.querySelector('.js-cat-info');
+const wrapperEl = document.querySelector('.js-wrapper');
+
+firstDownload();
+function firstDownload() {
+  wrapperEl.classList.remove('is-hidden');
+  fetchBreeds()
+    .then(data => {
+      data.map(breed => {
+        const option = document.createElement('option');
+        option.textContent = breed.name;
+        option.value = breed.id;
+        selectBreedEl.appendChild(option);
+      });
+      new SlimSelect({
+        select: selectBreedEl,
+      });
+    })
+    .catch(onError)
+    .finally(() => {
+      wrapperEl.classList.add('is-hidden');
+    });
+}
+
+selectBreedEl.addEventListener('change', changeBreed);
+
+function changeBreed(e) {
+  let breedIdCat = e.target.value;
+  wrapperEl.classList.remove('is-hidden');
+  fetchCatByBreed(breedIdCat)
+    .then(data => {
+      const markup = breedRender(data);
+      catInfo.innerHTML = markup;
+    })
+    .catch(() => {
+      onError;
+    })
+    .finally(() => {
+      wrapperEl.classList.add('is-hidden');
+    });
+}
+
+function breedRender(breeds) {
+  return breeds.map(breedMarkup).join('');
+}
+
+function onError() {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
+}
